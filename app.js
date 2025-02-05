86c68a6582ee2d6fb81b783211beadfc13bdd41f
mjavedjs <mjavedjs@2006@gmail.com>
2025-02-05 23:58:39 +0500

    firebase_app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { auth, db } from "./firebaseConfig.js"; 
-import { collection, addDoc ,query,where ,getDocs } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";    
+import { collection, addDoc ,query,where ,getDocs ,doc, deleteDoc , updateDoc ,} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js"; 
+import { onAuthStateChanged , signOut} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";    
 
 
 
@@ -9,7 +9,8 @@ let  todoArray = []
 onAuthStateChanged(auth, (user) => { 
     if (user) { 
         // console.log("User ID:", user.uid); // Display user ID when logged in
-        getDataFromFirestore()
+        getDataFromFirestore()  
+
     } else { 
         window.location = "login.html"; // Redirect to login if not authenticated
     }
@@ -18,19 +19,23 @@ onAuthStateChanged(auth, (user) => {
 
 form.addEventListener('submit', async (e) => {  
     e.preventDefault();  
-    const title = document.querySelector("#title").value;  
-    const description = document.querySelector("#description").value;  
+    let title = document.querySelector("#title").value;  
+    let description = document.querySelector("#description").value;  
 
     try {  
         const docRef = await addDoc(collection(db, "todo"), {  
             title: title,  
             description: description,  
-            uid: auth.currentUser.uid // Correct reference to user ID  
-        
-        });  
-        renderDataOnScreen(todoArray);
+            uid: auth.currentUser.uid 
+        });
 
-        // console.log("Document written with ID:", docRef.id);  
+        console.log("Document written with ID: ", docRef.id);
+        
+        document.querySelector("#title").value = "";  
+        document.querySelector("#description").value = "";  
+
+        getDataFromFirestore(); 
+
     } catch (e) {  
         console.error("Error adding document:", e);  
     }  
@@ -58,12 +63,60 @@ function renderDataOnScreen(arr){
         <tr>
         <td>${res.title}</td>
         <td>${res.description}</td>
-         <td> <button id="delbtn" onclick="deletMethod('${res.docid}')">Delete</button></td>
-         <td> <button id="delbtn" onclick="UpdatedtMethod('${res.docid}')">UPDate</button></td>
+         <td> <button class="delbtn">Delete</button></td>
+         <td> <button  class = "update-btn">UPDate</button></td>
          </tr>
      `
-     })
+     });
      table.innerHTML = html;
+
+     const updatebtn = document.querySelectorAll('.update-btn');
+     updatebtn.forEach((btn,index)=>{ 
+       
+      btn.addEventListener('click', async(e)=>{
+        let  newtitle = document.querySelector("#title").value = todoArray[index].title;
+        let newdescription = document.querySelector("#description").value = todoArray[index].description;
+        let id = todoArray[index].docid
+        try {
+            await updateDoc(doc(db, "todo", id), {
+                title:newtitle,
+                description:newdescription
+            });
+
+            console.log("Document updated successfully!");
+
+           
+
+        } catch (error) {
+            console.error("Error updating document:", error);
+        }
+         renderDataOnScreen(todoArray)
+      })
+ })
+
+ const delbtn = document.querySelectorAll('.delbtn');
+delbtn.forEach((btn,index)=>{ 
+ btn.addEventListener('click', async(e)=>{
+    const id = todoArray[index].docid;
+    await deleteDoc(doc(db, "todo", id)); // Delete from Firestore
+    todoArray.splice(index,1);
+    renderDataOnScreen(todoArray)
+ })
+})
+
 }
 
-renderDataOnScreen(todoArray)
+renderDataOnScreen(todoArray);
+
+
+const logoutbtn =document.querySelector('#logout');
+
+logoutbtn.addEventListener('click',()=>{
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      console.log('singout sucessfull')
+    }).catch((error) => {
+      // An error happened.
+      console.log(error)
+    });
+})
